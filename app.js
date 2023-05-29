26749b29c224d4c964659898e052cf8e6ebc76d1
MubbashirHussain <mubbashirhhussain@gmail.com>
2023-05-29 03:46:37 -0700

    JavaScript Shape shifter

---
+++ app.js
@@ -0,0 +1,17 @@
+var shape = document.getElementById("shape")
+
+function Circle(){
+shape.className = "Circle"
+}
+function traingle(){
+shape.className = "traingle"
+}
+function rectangle(){
+shape.className = "rectangle"
+}
+function oval(){
+shape.className = "oval"
+}
+function square(){
+shape.className = "square"
+}
